b752d80d7c34864c4d67742de5c0b27f2d2ee961
thecraigmcrae <thecraigmcrae@gmail.com>
2020-01-23 10:58:01 +0000

    rm line breaks and commas, add page title.
    added a function to strip commas and replace with a dash, the comments were not importing cleanly even while the entire block was in quotes.
    
    added pageName to pull the document title, useful for when pulling multiple forms into once csv and being able differentiate where feedback has originated.
    
    regex to remove line breaks from the comments.

--- comments_js.js
+++ comments_js.js
@@ -2,23 +2,29 @@ var csvArray = [],
 sectionName = "",
 userName = "",
 annotation = " ",
-comment = "";
-changed = 0;
+comment = "",
+changed = 0,
+pageName = document.title;
+
+function comma4dash(commaStr) {
+  return commaStr.replace(/,/g, '-'); //will replace commas for a dash
+}
 
 
 var elements = document.querySelectorAll('#ccx-comments-list li');
 Array.prototype.forEach.call(elements, function(el, i){
   if (el.classList.contains("global-section-header")) {
     sectionName = el.querySelectorAll(".title.truncated")[0].textContent;
-    csvArray.push(sectionName + ", - , - ,-" + "\n");
+    csvArray.push(pageName + "," + sectionName + ", - , - ,-" + "\n");
     return;
   }
   if (el.querySelectorAll(".user-name").length>0) {
-    userName = el.querySelectorAll(".user-name")[0].textContent;
+    userName = el.querySelectorAll(".user-name")[0].childNodes[0].textContent;
     changed = 1;
   }
   if (el.querySelectorAll(".comment-text").length>0) {
     comment = el.querySelectorAll(".comment-text span div")[0].textContent;
+    comment = comment.replace(/(\r\n|\n|\r)/gm,""); //remove line breaks
     changed = 1;
   }
   if (el.querySelectorAll(".annotation-marker").length>0) {
@@ -26,7 +32,7 @@ Array.prototype.forEach.call(elements, function(el, i){
     changed = 1;
   } else (annotation = " ");
 
-  changed?(csvArray.push(sectionName + "," + userName + ",\"" + comment + "\"," + annotation + "\n"), changed=0):'';
+  changed?(csvArray.push(pageName + "," + comma4dash(sectionName) + "," + comma4dash(userName) + ",\"" + comma4dash(comment) + "\"," + annotation + "\n"), changed=0):'';
 
 });
 
